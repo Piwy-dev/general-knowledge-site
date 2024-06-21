087288582531d9e53bf23c9e74a04cb451c328b6
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2024-06-21 19:37:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Website for the general knowledge bot.
+
+## Run the website
+```code
+. .venv/bin/activate
+```
+
+```code
+flask run
+```
---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, g, current_app, redirect, request
+
+def create_app(test_config=None):
+    """
+    Create and configure the app.
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    lang = 'en'
+
+   # create the pages
+    @app.route("/")
+    def index():
+        print(request.accept_languages)
+        return redirect('/{}/home'.format(lang))
+        
+    return app
